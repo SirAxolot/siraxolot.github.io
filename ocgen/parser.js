9b9d42989f11f1547ddaa848493480d52ae4bc8c
Axol <89815386+SirAxolot@users.noreply.github.com>
2022-03-02 08:54:50 -0500

    commit

---
+++ ocgen/parser.js
@@ -0,0 +1,17 @@
+function RandInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function splitarray(inp)
+{  
+    var text = fs.readFileSync("./" + inp + ".txt").toString('utf-8');
+    return text.split(",");
+}
+
+var nouns = splitarray("nouns");
+var adj = splitarray("adj");
+var verb = splitarray("actions");
+var sentences = splitarray("sentences");
+var rng = Date.now;
+const myElement = document.getElementById("field");
+myElement.innerText = nouns[RandInt(nouns.length)];
